7f035937d296ca2f132d54768425aa074825dc85
kebetu <ghislain.sambou@hotmail.com>
2019-03-15 23:18:21 +0100

    Push day00

---
+++ Day00/ex06/git_commit.sh
@@ -0,0 +1,4 @@
+# Il y a un truc magique qui indique à ta machine que tu travailles sur du shell
+# Et même si ça fonctionne très bien sans prend le temps d'apprivoiser "#!/bin/sh"
+# Premier indice : "git log" renseigne toi bien sur ses options et son fonctionnement.
+Tu as plein d'excellentes pages de man qui en parle sur internet.#
---
+++ Day00/ex07/git_ignore.sh
@@ -0,0 +1,4 @@
+# Afficher les fichiers qui sont traités comme ignorés... pas une tache facile...
+# Pour le coup suit le lien -> https://git-scm.com/docs/git-ls-files
+# Promis y a vraiment toutes les réponses !
+# Petit bonus des commandes git essentielles : https://www.hostinger.fr/tutoriels/commandes-git/
